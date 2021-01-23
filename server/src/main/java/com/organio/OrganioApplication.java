41107dd9b88d21c186813e43ad5579a9894e6554
pankeny <konrad_malocha@hotmail.com>
2021-01-23 21:20:36 +0100

    initial commit

---
+++ server/src/main/java/com/organio/OrganioApplication.java
@@ -0,0 +1,13 @@
+package com.organio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganioApplication.class, args);
+	}
+
+}

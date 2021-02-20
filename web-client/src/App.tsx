1c0401924d941759c50c1ed2e00ef26fb4ea15d4
pankeny <konrad_malocha@hotmail.com>
2021-02-20 22:01:53 +0100

    basic react project cleanup after init

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Context} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SecurityContext: Context<any> = React.createContext("");
+
+const App = () => {
+    return (
+        <SecurityContext.Provider value={"test"}>
+            <h1>Organio</h1>
+        </SecurityContext.Provider>
+    );
 }
 
 export default App;

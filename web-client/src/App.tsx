3ecba6d496f3c7abd68d70a76363313ea0404a28
pankeny <konrad_malocha@hotmail.com>
2021-03-01 21:38:38 +0100

    ORG-2 rearrange elements to get general layout

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,41 +1,88 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Button, Col, Container, Row} from "react-bootstrap";
-
+import {Col, Container, Row} from "react-bootstrap";
 
 const App = () => {
     const [toggled, toggle] = useState(true);
     const toggleSidebar = () => {
+        console.log("toggle")
         toggle(!toggled)
     }
 
     return (
-        <div className={toggled ? "menuDisplayed" : ""} id="wrapper">
-            <div id="sidebar-wrapper">
-                <ul className="sidebar-nav">
-                    <li>
-                        <a href="#">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Link 3</a>
-                    </li>
-                </ul>
+        <div className={!toggled ? "body-pd" : ""}>
+            <header className={toggled ? "header" : "header body-pd"} id="header">
+                <div className="header__toggle">
+                    <i
+                        className={toggled ? "bx bx-menu" : "bx bx-menu bx-x"}
+                        id="header-toggle"
+                        onClick={toggleSidebar}
+                    />
+                </div>
+
+                <div className="header__img">
+                    <i className='bx bx-layer' id=""/>
+                </div>
+            </header>
+
+            <div className={toggled ? "l-navbar" : "l-navbar show"} id="nav-bar">
+                <nav className="nav">
+                    <div>
+                        <a href="#" className="nav__logo">
+                            <i className='bx bx-layer nav__logo-icon'/>
+                            <span className="nav__logo-name">Organio</span>
+                        </a>
+
+                        <div className="nav__list">
+                            <a href="#" className="nav__link active">
+                                <i className='bx bx-grid-alt nav__icon'/>
+                                <span className="nav__name">Dashboard</span>
+                            </a>
+
+                            <a href="#" className="nav__link">
+                                <i className='bx bx-user nav__icon'/>
+                                <span className="nav__name">Users</span>
+                            </a>
+
+                            <a href="#" className="nav__link">
+                                <i className='bx bx-message-square-detail nav__icon'/>
+                                <span className="nav__name">Messages</span>
+                            </a>
+
+                            <a href="#" className="nav__link">
+                                <i className='bx bx-bookmark nav__icon'/>
+                                <span className="nav__name">Favorites</span>
+                            </a>
+
+                            <a href="#" className="nav__link">
+                                <i className='bx bx-folder nav__icon'/>
+                                <span className="nav__name">Data</span>
+                            </a>
+
+                            <a href="#" className="nav__link">
+                                <i className='bx bx-bar-chart-alt-2 nav__icon'/>
+                                <span className="nav__name">Analytics</span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <a href="#" className="nav__link">
+                        <i className='bx bx-log-out nav__icon'/>
+                        <span className="nav__name">Log Out</span>
+                    </a>
+                </nav>
             </div>
 
-                <Container fluid id="content-wrapper">
-                    <Button onClick={toggleSidebar}>Toggle</Button>
-                    <Row id="main-row">
-                        <Col md={5} className="brdr">
-                            <b>Todos</b>
-                        </Col>
-                        <Col md={7} className="brdr">
-                            <b>Calendar</b>
-                        </Col>
-                    </Row>
-                </Container>
+            <Container fluid >
+                <Row>
+                    <Col md={5}>
+                        <b>Todos</b>
+                    </Col>
+                    <Col md={7}>
+                        <b>Calendar</b>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }

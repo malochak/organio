38cb5d09a668366520722ce2698821cce61a30d8
Konrad Małocha <konrad_malocha@hotmail.com>
2021-11-21 20:15:30 +0100

    TG-2 #in-progress : implement login component skeleton

--- web-client/src/app/authentication/authentication.module.ts
+++ web-client/src/app/authentication/authentication.module.ts
@@ -3,17 +3,26 @@ import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login/login.component';
 import {AuthenticationRoutingModule} from "./authentication-routing.module";
 import {MatCardModule} from "@angular/material/card";
-
+import {LoginFormComponent} from './forms/login-form/login-form.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule,
-    MatCardModule,
-  ]
+    imports: [
+        CommonModule,
+        AuthenticationRoutingModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+    ]
 })
 export class AuthenticationModule {
 }
